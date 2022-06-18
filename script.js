ab3944284445fb98be65c10ee2c85ed15f95d019
Zachary Soliman <zachsoliman1@gmail.com>
2022-06-18 14:33:45 -0400

    home, about, project, and contact working individually

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ h2.textContent = "";
 
 //document.querySelector("body").appendChild(h2);
 
+/*
 let con = document.getElementById("btnContact"); //Target blank wasn't working
 btnContact.addEventListener("click", function () {
     window.open("contact.html", "_blank")
 });
+*/ //opens contact page in new tab but also opens in the already open tab
 
 let form = document.getElementById("contactForm");
 form.addEventListener("submit", function () {
-    alert("hello " + `${fname.value}`)
+    alert("Looking forward to speaking to you soon " + `${fname.value}`)
 });
+
