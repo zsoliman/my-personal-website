82dda4a6861a7c717abe296906c3dd8350acc4fc
Zachary Soliman <zachsoliman1@gmail.com>
2022-06-18 00:34:31 -0400

    Basic layout, linked contact page in new tab

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "";
 
-document.querySelector("body").appendChild(h2);
+//document.querySelector("body").appendChild(h2);
+
+let con = document.getElementById("btnContact"); //Target blank wasn't working
+btnContact.addEventListener("click", function () {
+    window.open("contact.html", "_blank")
+});
+
+let form = document.getElementById("contactForm");
+form.addEventListener("submit", function () {
+    alert("hello " + `${fname.value}`)
+});
